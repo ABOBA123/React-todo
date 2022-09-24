8159a3eb989129b7a498e2a3f5a0d8e9556d4559
ABOBA123 <tarasovlev7@gmail.com>
2022-09-24 20:30:27 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [backgraung, setbackgraung] = useState("white-theme")
+  return <div>
+    <div id="backgraung-color" class="backgraung-color" > {setbackgraung}
+    <p class="text-clicker">Count: {count}</p>
+    <button class="clicker" onClick={() => setCount((c) => c + 1)}> click me!</button>
+    <button class="white-theme" onClick={() => setbackgraung((c) => "white-theme")}>Смена темы</button>
+    <button class="black-theme" onClick={() => setbackgraung((c) => "black-theme")}>Смена темы</button>
+  </div>
+  </div>;
 }
 
 export default App;
