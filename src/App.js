ce924cf4f0bcbcb75850e854900a84f10a72758b
evyz <endermainYT2502@gmail.com>
2022-09-24 21:34:14 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,78 +1,33 @@
-import { useState, useEffect } from "react";
-import TextComponent from "./components/system/text/Text";
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [isLigth, setIsLigth] = useState(true);
+  //   2. Создайте форму кредитной карты
 
-  const [timeObj, setTimeObj] = useState(null);
+  //   Что вы узнаете, решив задачу:
 
-  useEffect(() => {
-    let date = new Date();
-    let hours = date.getHours();
-    if (hours > 19 || hours < 7) {
-      setIsLigth(false);
-    } else {
-      setIsLigth(true);
-    }
-  }, []);
-
-  // Создать новый useEffect, в массиве зависимостей которого будет timeObj
-  // В котором мы обновляем дату для вывода на сайте. То есть свои часы на сайте
+  // Как обрабатывать и проверять формы.
+  // Научитесь управлять слушателями событий (например, когда поля изменяются, он печатает значения на кредитной карте).
+  // Разберётесь с отображением и размещением элементов на странице, особенно данных кредитной карты, которые перекрывают форму.
 
   return (
     <div>
-      <div
-        id='backgraung-color'
-        className='backgraung-color'
-        style={{ background: isLigth ? "#FFFFFF" : "#282c34" }}
-      >
-        <h1>
-          {timeObj?.hours} : {timeObj?.minutes} : {timeObj?.seconds}
-        </h1>
+      <div>
+        <div className='card'>
+          <h1 className='card-title-bank'>BANK</h1>
 
-        <p
-          className='text-clicker'
-          style={{ color: isLigth ? "black" : "white" }}
-        >
-          Count: {count}
-        </p>
-        <button className='clicker' onClick={() => setCount((c) => c + 1)}>
-          {" "}
-          click me!
-        </button>
-        <button className='white-theme' onClick={() => setIsLigth((c) => true)}>
-          Смена темы
-        </button>
-        <button
-          className='black-theme'
-          onClick={() => setIsLigth((c) => false)}
-        >
-          Смена темы
-        </button>
+          <span>#### #### #### ####</span>
 
-        <TextComponent
-          ctxEl={"h1"}
-          content={"Статья блаблабла"}
-          isLigth={isLigth}
-        />
-        <p>
-          <TextComponent
-            ctxEl={"desc"}
-            content={"Описание статье блабла"}
-            isLigth={isLigth}
-          />
-        </p>
-        <TextComponent
-          ctxEl={"author"}
-          content={"Антонов Ярослав"}
-          isLigth={isLigth}
-        />
-        <TextComponent
-          ctxEl={"author"}
-          content={"Антонов Ярослав"}
-          isLigth={isLigth}
-        />
+          <div className='user-info'>
+            <div>
+              <span>Card Holder</span>
+              <h3>######</h3>
+            </div>
+
+            <div>
+              <span>Expires</span>
+              <h3>## / ##</h3>
+            </div>
+          </div>
+        </div>
+        <div>{/* Твоя вёрстка с данными карты тут */}</div>
       </div>
     </div>
   );
